cd949f7b239cc0bf6e69bdee4f16a24a69c608f5
numchoke <numchoke23@gmail.com>
2020-02-07 16:29:07 +0700

    07-02-2020

--- connect.php
+++ connect.php
@@ -2,14 +2,15 @@
     $servername = "localhost";
     $username = "root";
     $password = "BbilkB414148";
-    $dbname = "sotl";
- 
+    $dbname = "thrf14";
+
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
         echo "เชื่อมต่อสำเร็จ - DB : ".$dbname; 
+
         }
     catch(PDOException $e)
         {
--- workshop2.php
+++ workshop2.php
@@ -61,129 +61,21 @@
 </head>
 
 <body id="page-top">
-  <a id="button"></a>
-  <?php include "navbar.html"; ?>
-  <?php include "connect.php"; ?>
-  <!-- Workshop -->
-  <section class="bg-light page-section">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12 text-center mb-2">
-          <h2 class="section-heading text-uppercase">Workshops</h2>
-          <!-- <h3 class="section-subheading text-muted"> </h3> -->
-        </div>
+  <div class="form-row col-12 mt-2">
+    <div class="card col-md-8 col-sm-12">
+      <div class="card-header bg-info">
+        Upload บทความตีพิมพ์
       </div>
-      <!-- day 1 -->
-      <div class="row mb-2">
-        <div class="col-md-12 mark4 mb-2">
-          <h5 class="section-heading pt-2">วันที่ 30 เมษายน 2563 (เวลา 13.00 - 14.30 น.)</h5>
-        </div>
+      <form method="post" name="Form" action="check_upload.php" enctype="multipart/form-data" onSubmit="return validate();">
+      <div class="card-body">
+        <!-- <h5 class="card-title">Upload บทความตีพิมพ์</h5> -->
+        <!-- <p class="card-text">** maximum 2MB. Type .doc .docx </p> -->
+        <input type="file" name="paper_upload" id="paper_upload" />
+        <p class="card-text">**ไฟล์ขนาดไม่เกิน 2MB. Type .doc .docx </p>
+        <span id="file_error"></span>
+      <input type="submit"  name="submit" value="ok">
       </div>
-      <div class="row ml-4 mb-4">
-        <div class="col-12">
-          <h6>Workshop1 : Design Thinking </h6>
-        </div>
-        <p class="text-muted ml-4">ผู้ช่วยศาสตราจารย์ ดร. สุธารัตน์ โชติกประคัลภ์ และ อ. นัดดาวดี นุ่มนาค
-          ฝ่ายวิชาบูรณาการ หมวดวิชาศึกษาทั่วไป มหาวิทยาลัยเกษตรศาสตร์</p>
-        <a class="btn btn-sm btn-regis ml-4 mb-3 d-inline" href="#" data-toggle="modal"
-          data-target="#exampleModal">รายชื่อผู้ลงทะเบียน Workshop</a>
-        <hr>
-        <div class="col-12">
-          <h6>Workshop2 : Research to Innovation</h6>
-        </div>
-        <p class="text-muted ml-4">ผู้ช่วยศาสตราจารย์ ดร. วรพันธ์ คู่สกุลนิรันดร์ คณะเทคโนโลยีสารสนเทศและการสื่อสาร
-          มหาวิทยาลัยมหิดล</p>
-        <div class="col-12">
-          <h6>Workshop3 : Contemplative education: Learn Unlearn and Relearn</h6>
-        </div>
-        <p class="text-muted ml-3">รองศาสตราจารย์ นายแพทย์ชัชวาลย์ ศิลปกิจ ผู้อำนวยการศูนย์จิตตปัญญาศึกษา
-          มหาวิทยาลัยมหิดล</p>
-        <div class="col-12">
-          <h6>Workshop4 : Development and Implementation-Game design-Board game</h6>
-        </div>
-        <p class="text-muted ml-3">ดร. เดชรัต สุขกำเนิด หัวหน้าภาควิชาเศรษฐศาสตร์เกษตรและทรัพยากร มหาวิทยาลัยเกษตรศาสตร์
-        </p>
-      </div>
-      <!-- day 2 -->
-      <div class="row mb-2">
-        <div class="col-md-12 mark4 mb-2">
-          <h5 class="section-heading  pt-2">วันที่ 1 พฤษภาคม 2563 (เวลา 14.45 - 16.00 น.)</h5>
-        </div>
-      </div>
-      <div class="row ml-4 ">
-        <div class="col-12">
-          <h6>Workshop5 : เนรมิตห้องเรียน 4.0 ด้วยเครื่องมือดิจิทัล</h6>
-        </div>
-        <p class="text-muted ml-3">ดร. กุลชัย กุลตวนิช ผู้ช่วยคณบดีคณะเทคโนโลยีการเกษตร
-          สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</p>
-        <div class="col-12">
-          <h6>Workshop6 : Micro mastery: ประสบการณ์การพัฒนาการเรียนรู้เพื่อสร้าง “ความเก่งทีละเล็กทีละน้อย</h6>
-        </div>
-        <p class="text-muted ml-3">ดร. ก้องกาญจน์ กาญจนพนัง คณะศิลปศาสตร์ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี</p>
-        <div class="col-12">
-          <h6>Workshop7 : วิจัยจัดการเรียนการสอน</h6>
-        </div>
-        <p class="text-muted ml-3">ผู้ช่วยศาสตราจารย์ ดร. จิตต์ภิญญา ชุมสาย ณ อยุธยา ผู้อำนวยการสำนักนวัตกรรมการเรียนรู้
-          มหาวิทยาลัยศรีนครินทรวิโรฒ</p>
-        <div class="col-12">
-          <h6>Workshop8 : สร้างห้องเรียนแห่งอนาคตด้วยเทคโนโลยี iPad และ Personalization of Learning</h6>
-        </div>
-        <p class="text-muted ml-3">คุณปัญชสา เชาวน์ชูเวชช Apple Professional Learning Provider UFicon Co., Ltd.</p>
-      </div>
-
-    </div>
-    </div>
-  </section>
-  <!-- Footer -->
-  <?php 
-  include "footer.html";
-  ?>
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Workshop 1 : Design Thinking</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <table class="table table-striped">
-            <thead>
-              <tr>
-                <!-- <th scope="col">ลำดับ</th>
-                <th scope="col">ชื่อ-สกุล</th>
-                <th scope="col">สังกัด</th> -->
-
-                <th scope="col" width="10%">ลำดับ</th>
-                <th scope="col" width="30%">ชื่อ-สกุล</th>
-                <th scope="col" width="60%">สังกัด</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <?php
-                    $i = 1;
-                    $stmt = $conn->query("SELECT id,regis_title_name,regis_th_name,regis_th_lastname,regis_affiliation FROM register where regis_payment_status != 9 order by id ASC");
-                    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-                ?>
-              <tr>
-                <th scope="row" class="text-center"><?php echo $i; ?></th>
-                <td><?php echo  $row['regis_title_name'].$row['regis_th_name']." ".$row['regis_th_lastname'];?></td>
-                <td><?php echo $row['regis_affiliation'];?></td>
-              </tr>
-              <?php $i++;  } ?>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
-        </div>
-      </div>
+      </form>
     </div>
   </div>
 
@@ -195,6 +87,19 @@
   <script src="js/agency.min.js"></script>
   <script src="js/top_page.js"></script>
   <script src="js/check_regis.js"></script>
+  <script>
+function validate() {
+	$("#file_error").html("");
+	$(".demoInputBox").css("border-color","#F0F0F0");
+	var file_size = $('#paper_upload')[0].files[0].size;
+	if(file_size>2097152) {
+		$("#file_error").html("<font color='#F31616'>รูปภาพของท่านมีขนาดใหญ่เกิน 2 MB</font>");
+		$(".demoInputBox").css("border-color","#FF0000");
+		return false;
+	} 
+	return true;
+}
+</script>
 
 </body>
 
