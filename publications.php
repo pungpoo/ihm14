5d0221955cfa8ff0a9ac20f917147838743c1f91
TUM-Numchoke <pungpoo23@gmail.com>
2020-02-16 02:16:23 +0700

    new Menu

---
+++ publications.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>SOTL6</title>
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet'
+    type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+  <!-- Custom styles for this template -->
+  <link href="css/agency.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <style>
+    @media (min-width: 992px) {
+      section {
+        margin-top: 100px;
+      }
+    }
+
+    @media (max-width: 991px) {
+      section {
+        margin-top: 100px;
+      }
+    }
+
+    @media (min-width: 320px) and (max-width: 480px) {
+      section {
+        /* padding: 80px 0 !important; */
+        margin-top: 50px;
+      }
+    }
+
+    .bg-2nd {
+      background-color: #FEF5E7;
+    }
+  </style>
+</head>
+
+<body id="page-top">
+  <a id="button"></a>
+  <?php include "navbar.html"; ?>
+  <!-- Workshop -->
+  <section class="bg-light page-section" style="margin-bottom: 150px;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center mb-2">
+          <h2 class="section-heading text-uppercase">หลักเกณฑ์การส่งผลงาน</h2>
+          <!-- <h3 class="section-subheading text-muted"> </h3> -->
+        </div>
+      </div>
+      <!-- day 1 -->
+      <div class="row mb-2">
+        <div class="col-md-12 mark4 mb-2">
+          <h5 class="section-heading text-center lh pt-2">ประกาศรับบทความวิชาการ/บทความวิจัย (รอบแรก) <br>
+            เพื่อพิจารณาสำหรับการนำเสนอในการประชุมวิชาการระดับชาติ <br>
+            เวทีวิจัยมนุษยศาสตร์ไทย ครั้งที่ 14 "iHumanities: เทคโนโลยี สุขภาพ และชีวิต" <br>
+            ระหว่างวันที่ 8-9 กันยายน 2563 ณ มหาวิทยาลัยมหิดล ศาลายา นครปฐม
+          </h5>
+        </div>
+      </div>
+      <div class="row ml-4">
+        <div class="col-12 ">
+          <h6 class="lh mt-4">
+            1. เปิดรับบทความวิชาการ/บทความวิจัยฉบับเต็ม ตามหัวข้อย่อยที่กำหนด ลักษณะของบทความ ได้แก่ <br>
+            <li class="ml-4">มีบทคัดย่อภาษาไทยและภาษาอังกฤษ</li>
+            <li class="ml-4">มีความยาว 15 หน้ากระดาษ A4 (รวมบรรณานุกรม) ใช้ Font Thai Saraban PSK ขนาด 16</li>
+            <li class="ml-4">ใช้วิธีการเขียนอ้างอิงและบรรณานุกรมแบบ APA</li>
+            2. กำหนดรับบทความ ตั้งแต่วันนี้ จนถึงวันที่ 17 เมษายน พ.ศ. 2563 <br>
+            3. ส่งไฟล์ Microsoft Word ของบทความฉบับสมบูรณ์และสอบถามรายละเอียดเพิ่มเติมได้ที่ อีเมล์: <red>
+              ihumanities14@gmail.com</red> <br><br>
+          </h6>
+
+          <h6 class="lh"><red>***สำหรับผู้ที่ผ่านการคัดเลือกเพื่อนำเสนอบทความ
+            จะได้รับทุนสนับสนุนการเดินทาง ที่พัก และอาหารจากทาง สกสว.***</red>
+          </h6>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+  
+  <!-- Footer -->
+  <?php 
+  include "footer.html";
+  ?>
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <script src="js/agency.min.js"></script>
+  <script src="js/top_page.js"></script>
+  <script src="js/check_regis.js"></script>
+
+</body>
+
+</html>
