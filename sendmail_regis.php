84d9e8c81812846f664259c425036cbb163dc8e3
pungpoo <pungpoo23@gmail.com>
2020-08-24 16:45:56 +0700

    Update sendmail_regis.php

--- sendmail_regis.php
+++ sendmail_regis.php
@@ -14,7 +14,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'ihumanities14@gmail.com';                     // SMTP username
-    $mail->Password   = 'shmahidol2020';                               // SMTP password
+    $mail->Password   = '##';                               // SMTP password
     $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
